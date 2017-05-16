2d0b46bbc43e414d84116d4278c9edf0d4c06722
rkashaev <kashaev.rr@gmail.com>
2017-05-16 21:40:40 +0300

    file reader

---
+++ src/main/java/courses/lesson14/FileReaderExample.java
@@ -0,0 +1,24 @@
+package courses.lesson14;
+
+import java.io.*;
+import java.net.URISyntaxException;
+
+public class FileReaderExample {
+    public static void main(String[] args) throws URISyntaxException {
+//        File file = new File(cls.getResource("wiki.html").toURI());
+
+        try (BufferedReader br = new BufferedReader(getResource("wiki.html"))) {
+            while (br.ready()) {
+                String string = br.readLine();
+                System.out.println(string);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static InputStreamReader getResource(String name) {
+        Class<FileReaderExample> cls = FileReaderExample.class;
+        return new InputStreamReader(cls.getResourceAsStream(name));
+    }
+}
