a036e910b0a228a28d07dc883ec4e81fbe02b587
rkashaev <kashaev.rr@gmail.com>
2017-05-25 19:43:59 +0300

    bracket tests

---
+++ src/test/java/courses/tasks/BracketsTest.java
@@ -0,0 +1,31 @@
+package courses.tasks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BracketsTest {
+
+    @Test
+    public void testCheck() {
+        assertEquals(true,  Brackets.check(""));
+        assertEquals(true,  Brackets.check("s"));
+        assertEquals(true,  Brackets.check("(dsf sdfs)"));
+        assertEquals(true,  Brackets.check("[sdfsdf]"));
+        assertEquals(true,  Brackets.check("{}"));
+        assertEquals(true,  Brackets.check("{(a + d) +c}"));
+        assertEquals(true,  Brackets.check("{()[]()}"));
+        assertEquals(true,  Brackets.check("{()[{}]()}"));
+        assertEquals(true,  Brackets.check("{([])}"));
+        assertEquals(true,  Brackets.check("{([([])])}"));
+        assertEquals(true,  Brackets.check("[([([])])]"));
+        assertEquals(false, Brackets.check("[(]"));
+        assertEquals(false, Brackets.check("[(}]"));
+        assertEquals(false, Brackets.check("[)"));
+        assertEquals(false, Brackets.check("[)]"));
+        assertEquals(false, Brackets.check("}{"));
+        assertEquals(false, Brackets.check(")("));
+        assertEquals(false, Brackets.check("]["));
+    }
+
+}
