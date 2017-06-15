fad4d34b565d7b2f03113ee9a6e265b33a3449e6
rkashaev <kashaev.rr@gmail.com>
2017-06-15 21:45:07 +0300

    networking

---
+++ src/main/java/courses/net/Networking.java
@@ -0,0 +1,54 @@
+package courses.net;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+public class Networking {
+    public static void main(String[] args) throws IOException {
+        useSockets("horstmann.com", 80);
+
+//        useUrlConnection("horstman.com");
+    }
+
+    private static void useSockets(String host, int port) throws IOException {
+
+
+        try (Socket socket = new Socket(host, port);
+             PrintWriter pw = new PrintWriter(socket.getOutputStream());
+             InputStream is = socket.getInputStream();) {
+
+            pw.println("GET / HTTP/1.1");
+            pw.println("HOST: " + host);
+            pw.println();
+            pw.flush();
+
+
+            printFromInputStream(is);
+
+        }
+    }
+
+    private static void useUrlConnection(String urlStr) throws IOException {
+        URL url = new URL("http://" + urlStr);
+        URLConnection urlConnection = url.openConnection();
+
+        InputStream is = urlConnection.getInputStream();
+        printFromInputStream(is);
+
+        is.close();
+    }
+
+    private static void printFromInputStream(InputStream is) throws IOException {
+        Scanner sc = new Scanner(is);
+
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+        sc.close();
+    }
+}
